c4f51d604c4a37d9424f2504cf4353dcdbbaa330
mdselimme <mdselimdev@gmail.com>
2025-01-01 23:39:48 +0600

    add-c-programming-comment-method-and-print-method

--- Hello_World_C/Hello.c
+++ Hello_World_C/Hello.c
@@ -1,6 +1,14 @@
+//This line is header file for input and output function in C
 #include <stdio.h>
 
+/*
+In this file We will learn How to hello world in c programming langauge and also learn how to write single line comment and multiline comment 
+(//) -> this is stand for single line comment and We write text in multiline comment
+*/
+
+// This is main Funtion in Where cannot pass any parameter 
 int main(){
+    // This is method output in c programming 
     printf("Hello world");
     return 0;
 };
