e353f1ab58e4115cd34a6d154f18afd8088479a9
mdselimme <mdselimdev@gmail.com>
2025-01-03 01:09:06 +0600

    add-c-output-method

---
+++ 2.C_Output/output.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+    In this tutitorial we will learn how to print output in new line, horizontal tab, backslash and double quote;
+*/
+
+int main(){
+    printf("C output method\n");
+    // new line in c 
+    printf("My name is\nSelim");
+    //horizontal tab in c
+    printf("Abdul Alim is a\t doctor\n");
+    // single blackslash in c 
+    printf("This is\\ backslash\n");
+    // double quote in c 
+    printf("Dobult quote \"C\"");
+    return 0;
+}
