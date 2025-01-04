d7efb6bf7fc873b2da1cd6b7101b66571621c4fc
mdselimme <mdselimdev@gmail.com>
2025-01-04 18:00:00 +0600

    add-character-data-types

--- 4.C_Data_types/character.c
+++ 4.C_Data_types/character.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
+    //character data types can not accept more than one character and it's format specifires is %c. 
     printf("Character data types\n");
-    
+    char s = 50, y = 60, z = 30;//these variables print ASCII symbol
+    printf("character data types: s = %c, y = %c, z = %c;\n", s, y, z);
     return 0;
 }
