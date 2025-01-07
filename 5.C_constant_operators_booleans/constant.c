3f69b4e6211c7aa2005e832561d54ee058d233f1
mdselimme <mdselimdev@gmail.com>
2025-01-07 23:21:10 +0600

    add-operators

--- 5.C_constant_operators_booleans/constant.c
+++ 5.C_constant_operators_booleans/constant.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+/*
+
+    constant is a variable that you cannot change the value and you can only use it.
+*/
+
 int main(){
     printf("Constant variable in c\n");
+    const int numb= 10;
+    printf("%d", numb);
+    // numb = 12;constant variable cannot reasign value;
+    printf("%d", numb);
     return 0;
 }
---
+++ 5.C_constant_operators_booleans/operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+    There are five operators.
+    1. Arithmetic operators.asm
+    2. Assignment Operators
+    3. Comparison Operators
+    4. Logical Operators
+    5. Bitwise Operators
+*/
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
