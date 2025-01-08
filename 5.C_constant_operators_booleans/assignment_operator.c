f81f2e2f68ceb730b836c82331b6ddb6912c5e03
mdselimme <mdselimdev@gmail.com>
2025-01-08 23:16:23 +0600

    add-assignment-variable

---
+++ 5.C_constant_operators_booleans/assignment_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("Assignment Operator.\n");
+    //Assignment operator (=) that we called equal in math. It used in a variable to assign a value.
+    int x = 0;
+    // assignment operator (=) to assign the value 0 to a variable called x
+    printf("Assignment : %d", x);
+    return 0;
+}
