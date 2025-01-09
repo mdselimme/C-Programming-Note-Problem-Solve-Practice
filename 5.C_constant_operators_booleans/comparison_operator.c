6d1e6d23b72dd3494c5f003d957aef56a1bc66eb
mdselimme <mdselimdev@gmail.com>
2025-01-09 23:39:08 +0600

    add-comparison-operator

---
+++ 5.C_constant_operators_booleans/comparison_operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+    // if the comparison is true then it gives (1) answer otherwise gives (0);
+    int x = 10;
+    int y = 10;
+    // equal to 
+    printf("equal to %d\n", x == y);
+    // not equal 
+    printf("not equal %d\n", x != y);
+    // greater than
+    printf("greater than %d\n", x > y);
+    // less than 
+    printf("less than %d\n", x < y);
+    // greater than or equal
+    printf("greater than or equal %d\n", x >= y);
+    // less than or equal
+    printf("less than or equal %d\n", x <= y);
+    return 0;
+}
