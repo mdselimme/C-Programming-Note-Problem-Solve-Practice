79ece557263bd4ca8cab96e31d70885fd4b6a007
mdselimme <mdselimdev@gmail.com>
2025-01-10 23:25:47 +0600

    add-while-loop

---
+++ 8.Loop/while_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    // initial value 
+    int j = 0;
+    // condition
+    while (j < 10)      
+    {
+        printf("%d\n", j);
+        //counting
+        j++;
+    }
+    return 0;
+}
