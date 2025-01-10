5a41ca1b5bb9734e952d0b6d3eb1e8c5bddfaf98
mdselimme <mdselimdev@gmail.com>
2025-01-10 23:13:10 +0600

    add-ternary-operator

---
+++ 7.Conditional_Statements/ternary_opeator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("if else statement.\n");
+    int x , y;
+    x = 10;
+    y=10;
+    (x == y) ? printf("x is equal to y"): printf("y is greater than x");
+    return 0;
+}
