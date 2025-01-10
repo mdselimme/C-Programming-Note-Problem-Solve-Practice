d900fb1a5d511cf2dce20fe6e250f27fbedc2af4
mdselimme <mdselimdev@gmail.com>
2025-01-10 23:27:40 +0600

    add-do-while

---
+++ 8.Loop/do_while_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    // initial value 
+    int w = 0;
+    // work in do loop
+      do
+    {
+        printf("%d\n", w);
+        //counting
+        w++;
+    } while (w < 10);//condtion write in while
+    return 0;
+}
