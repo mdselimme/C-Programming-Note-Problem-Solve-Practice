a229e1a5c092a77fc50ba6e5bf967336f066f603
mdselimme <mdselimdev@gmail.com>
2025-01-12 23:50:59 +0600

    add-array-declare-method

---
+++ 9.Arrays/arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    printf("Hello Arrays.\n");
+    // declare an array 
+    int physics_number[] = {10, 20, 30, 40, 50, 60};
+    // print an array value in index number 
+    printf("%d", physics_number[0]);
+    return 0;
+}
