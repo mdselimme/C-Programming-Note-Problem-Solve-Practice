15c9e69e469ec8953992b09c670c48da4b33ebf4
mdselimme <mdselimdev@gmail.com>
2025-01-12 23:52:48 +0600

    add-array-change-value-method

--- 9.Arrays/arrays.c
+++ 9.Arrays/arrays.c
@@ -5,6 +5,8 @@ int main () {
     // declare an array 
     int physics_number[] = {10, 20, 30, 40, 50, 60};
     // print an array value in index number 
+    printf("%d\n", physics_number[0]);
+    physics_number[0] = 50;
     printf("%d", physics_number[0]);
     return 0;
 }
