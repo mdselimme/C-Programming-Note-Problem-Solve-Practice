967d9d97c3cb76eeccc56bcf3f408f33c579b3d9
mdselimme <mdselimdev@gmail.com>
2025-01-12 23:57:47 +0600

    add-array-size-method

--- 9.Arrays/arrays.c
+++ 9.Arrays/arrays.c
@@ -7,6 +7,15 @@ int main () {
     // print an array value in index number 
     printf("%d\n", physics_number[0]);
     physics_number[0] = 50;
-    printf("%d", physics_number[0]);
+    printf("%d\n", physics_number[0]);
+    // set array size 
+    int num[4];
+    // assign value in array 
+    //also don't mixing data type like type is integer you have to assign integer value 
+    num[0] = 5;
+    num[1] = 6;
+    num[2] = 7;
+    num[3] =8;
+    printf("%d", num[0]);
     return 0;
 }
