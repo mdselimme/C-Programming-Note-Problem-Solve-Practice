56ae02180c4f93f4d6b935b60bb5bc81cca1d8ec
mdselimme <mdselimdev@gmail.com>
2025-01-14 00:33:16 +0600

    add-special-character-in-string-c

--- 10.String/ano_method_str.c
+++ 10.String/ano_method_str.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main () {
-
     // declare a string like an array 
     char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
     char greetings2[] = "Hello World!";
@@ -9,6 +8,5 @@ int main () {
     printf("%d\n", sizeof(greetings));
     printf("%s\n", greetings2);
     printf("%d\n", sizeof(greetings2));
-
     return 0;
 }
---
+++ 10.String/spe_character_str.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("Special Character.\n");
+    printf("\\Special\\ \"Character\" \'in\' string.\n");
+    printf("Special\n Escape\t Character.\0");
+    return 0;
+}
