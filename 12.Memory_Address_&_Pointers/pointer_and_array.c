706bb1d4587a9b6214d1dcba750fbb790b5e6160
mdselimme <mdselimdev@gmail.com>
2025-01-15 20:53:24 +0600

    add-change-array-value-and-print-by-pointer-variable

--- 12.Memory_Address_&_Pointers/pointer_and_array.c
+++ 12.Memory_Address_&_Pointers/pointer_and_array.c
@@ -7,5 +7,17 @@ int main () {
     for(int j = 0; j < length; j++){
         printf("Number is : %d - Memory Address is : %p.\n", myRoll[j], &myRoll[j]);
     };
+    //Change array value by pointers
+    char myNum[3] = {25,12,10};
+    *myNum = 5;
+    *(myNum + 1) = 14;
+    printf("%d\n", *myNum);
+    printf("%d\n", *(myNum+1));
+    //change array to pointer array
+    int myMoney[4] = {120,127,135,140};
+    int *ptr = myMoney;
+    for (int i = 0; i < 4; i++) {
+    printf("%d\n", *(ptr + i));
+}
     return 0;
 }
