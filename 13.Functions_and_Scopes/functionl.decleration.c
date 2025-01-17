a4aa0f6271fa407d167e69ba39f53137593af67f
mdselimme <mdselimdev@gmail.com>
2025-01-18 01:14:23 +0600

    add-fucntion-declaration

--- 13.Functions_and_Scopes/functionl.decleration.c
+++ 13.Functions_and_Scopes/functionl.decleration.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-
+// declare a function above main function and definition below main 
+// declare function 
+void myNumber(int x , int y);
+void myOtherNumber(int sum);
 
 int main () {
-    printf("Function Scope in C\n");
-      
+    printf("Function Declaration in C\n");
+    // call the function 
+    myNumber(10,5);
     return 0;
+}
+
+void myNumber(int x , int y){
+    printf("Hello decleration %d + %d = %d\n", x , y, x+y);
+    myOtherNumber(x+y);
+}
+
+void myOtherNumber(int sum){
+    printf("Hello Other decleration %d", sum);
 }
