15532bb32731acadcf107a8dfca710e6d8f7366c
mdselimme <mdselimdev@gmail.com>
2025-01-21 23:48:27 +0600

    add-file-create-and-write-content

--- 14.C-Files/write_in_file.c
+++ 14.C-Files/write_in_file.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main(){
-    printf("Create file in Pc by C program.\n");
+int main () {
     FILE *fptr;
+
     fptr = fopen("creat-file.txt", "w");
+
+    fprintf(fptr, "Create And Write file in C programming");
+
     fclose(fptr);
+
     return 0;
 }
