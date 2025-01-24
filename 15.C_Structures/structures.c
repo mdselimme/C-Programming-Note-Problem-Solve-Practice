8fb5093c5c695c38a067ecbe2c1a43ad264337ac
mdselimme <mdselimdev@gmail.com>
2025-01-24 21:21:04 +0600

    add-structers-define-method

--- 15.C_Structures/structures.c
+++ 15.C_Structures/structures.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
+// define a structures 
+struct myStructures
+{
+    int myNum;
+    char myChar;
+};
+
+
+
+int main () {
     printf("Hello World Structures.\n");
+    // write a structures variable name;
+    // assgin value in structers 
+    struct myStructures s1;
+    s1.myChar = 'A';
+    s1.myNum = 15;
+    // print structers value 
+    printf("Number is : %d\n", s1.myNum);
+    printf("Character is : %c", s1.myChar);
     return 0;
 }
