4cc237d81f9ec4db06f000caa1c345c3f60333b6
mdselimme <mdselimdev@gmail.com>
2025-01-24 23:30:46 +0600

    copy-structure

--- 15.C_Structures/structures_strings.c
+++ 15.C_Structures/structures_strings.c
@@ -6,7 +6,10 @@ struct myStringStructure{
 };
 
 int main () {
-    struct myStringStructure s1 = {2, "Hi Soleman. How are you ?"};
+    struct myStringStructure s1 = {2, "Hi Soleman. How are you ?\n"};
     printf("number : %d, message : %s", s1.num, s1.myStr);
+    struct myStringStructure s2;
+    s2 = s1;
+      printf("s2 number : %d, message : %s", s2.num, s2.myStr);
     return 0;
 }
