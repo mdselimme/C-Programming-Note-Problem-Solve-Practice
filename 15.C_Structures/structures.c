87248afe1a50d87db312f86bc09232a82d8b43cc
mdselimme <mdselimdev@gmail.com>
2025-01-24 23:21:15 +0600

    add-structure-2

--- 15.C_Structures/structures.c
+++ 15.C_Structures/structures.c
@@ -14,10 +14,15 @@ int main () {
     // write a structures variable name;
     // assgin value in structers 
     struct myStructures s1;
+    struct myStructures s2;
     s1.myChar = 'A';
     s1.myNum = 15;
+    s2.myChar = 'B';
+    s2.myNum = 20;
     // print structers value 
     printf("Number is : %d\n", s1.myNum);
-    printf("Character is : %c", s1.myChar);
+    printf("Character is : %c\n", s1.myChar);
+    printf("Number is s2 : %d\n", s2.myNum);
+    printf("Character is s2 : %c\n", s2.myChar);
     return 0;
 }
